ae7a0d6c64c090b1de53e8b2fbc94223bf6be07f
Aditi Nair <addynair6@gmail.com>
2023-09-10 18:24:23 +0530

    Created bot, requirements.txt and scarper.py

--- task6/bot.py
+++ task6/bot.py
@@ -1,31 +1,45 @@
 import discord
 import os
+import random
 from dotenv import load_dotenv 
 import requests
+from discord.ext import commands
 
 load_dotenv()
 
-
-client = discord.Client(intents=discord.Intents.default())
-
-def get_score():
-    response = requests.get("")
-
+intents=discord.Intents.default()
+client = discord.Client(intents = intents)
+intents.message_content = True
+token = os.getenv('TOKEN')
 
 @client.event
-
 async def on_ready():
-    print('We have logged in as {0.user}'
-          .format(client))
-    
+    print('We have logged in as {0.user}'.format(client))
+
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
     
-    if message.content == 'hello':
-        print("said hello")
+    if message.content.startswith('hello'):
+        print("ACTIVE")
+        # await client.process_commands('hello')
         await message.channel.send('Hello!')
+
+
+
+    
+#discord_bot = commands.Bot(command_prefix='!')
+#@discord_bot.command(name='livescore')
+#async def msg(ctx):
+#     quotes="be positive"
+
+#     response = random.choice(quotes)
+#     await ctx.send(response)
+client.run(token)
+
     
 
-client.run(os.getenv('TOKEN'))
+    
+
