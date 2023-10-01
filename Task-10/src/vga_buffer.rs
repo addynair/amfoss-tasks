367a3d684940d326103c2be6bd14541185c13e0e
Aditi Nair <addynair6@gmail.com>
2023-10-01 14:47:15 +0530

    Minor change in vga_buffer file

--- Task-10/src/vga_buffer.rs
+++ Task-10/src/vga_buffer.rs
@@ -19,11 +19,11 @@ lazy_static! {
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 #[repr(u8)]
 pub enum Color {
-    Black = 4,
+    Black = 0,
     Blue = 1,
     Green = 2,
     Cyan = 3,
-    Red = 0,
+    Red = 4,
     Magenta = 5,
     Brown = 6,
     LightGray = 7,
